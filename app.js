7750761e9ef793a7d63ceff6e4be6d10564a7f68
chaggysen <sen.wang@mail.mcgill.ca>
2020-05-23 13:13:26 -0400

    implement cart values and display cart

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const productsDOM = document.querySelector(".products-center");
 
 // cart
 let cart = [];
+// buttons
+let buttonsDOM = [];
 
 // Product class: getting the products:
 // This will be responsible for getting the products localy first,
@@ -95,9 +97,14 @@ class UI {
   // The method iterate through all buttons and retrive their ids.
   // We find the items that are already inCart by using find method on the cart array.
   // If the item is already in cart, we change the innnerHTML of the button to "In cart" and we disable the button.
-  // Else if the add to bag button is clickec, we change the innerText to "In cart" and we disable the button.
+  // If the add to bag button is clicked, we change the innerText to "In cart" and we disable the button.
+  // We then get each product from products and add product to the cart and save cart in local storage.
+  // After, we set the cart values and display the item in the cart.
+  // Finally, each time we add an item to the cart, it shows the cart.
   getBagButtons() {
+    // The spread operator(...) with objects is used to make a copy of an existing object or to make new object with more properties
     const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
     buttons.forEach((button) => {
       let id = button.dataset.id;
       // The find() method returns the value of the first element in the provided array that satisfies the provided testing function.
@@ -105,14 +112,74 @@ class UI {
       if (inCart) {
         button.innerHTML = "In cart";
         button.disabled = true;
-      } else {
-        button.addEventListener("click", (event) => {
-          // The target property of the event interface is a reference to the object onto which the event was dispached.
-          event.target.innerText = "In cart";
-          event.target.disabled = true;
-        });
       }
+      button.addEventListener("click", (event) => {
+        // The target property of the event interface is a reference to the object onto which the event was dispached.
+        event.target.innerText = "In cart";
+        event.target.disabled = true;
+        // get product from products. The id is already defined with dataset attribute.
+        // Here we are declaring an object. We are creating an array of all the properties that Storage.getProducts(id) is returning
+        // and we all another amount property with 1 as default.
+        let cartItem = { ...Storage.getProduct(id), amount: 1 };
+        // add product to the cart array.
+        // Here, we get all the items we had in the cart array currently and add a new cartItem
+        cart = [...cart, cartItem];
+        // save cart in local storage because we still want the same cart when I refresh.
+        Storage.saveCart(cart);
+        // set cart values
+        this.setCartValues(cart);
+        // display cart items
+        this.addCartItem(cartItem);
+        // show the cart
+        this.showCart();
+      });
     });
+  }
+
+  // This method will update both the total price and the total amount of each item in the cart array.
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map((item) => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    // Here we update the values in the ineerText.
+    // The parseFloat function parses a string and returns a floating point number
+    // toFixed (we want 2 decimals)
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+
+  // This method will add the item in the dom.
+  // It creates a new div and add it to the cart-item class in HTML.
+  // The item.id allows us to dynamically change the amount of a specific item
+  addCartItem(item) {
+    // Here we create the item to be added with a div
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `
+    <img src=${item.image} alt="product" />
+            <div>
+              <h4>${item.title}</h4>
+              <h5>${item.price}$</h5>
+              <span class="remove-item" data-id =${item.id}>remove</span>
+            </div>
+            <div>
+              <i class="fa fa-chevron-up" data-id =${item.id}></i>
+              <p class="item-amount">${item.amount}</p>
+              <i class="fa fa-chevron-down" data-id =${item.id}></i>
+            </div>
+    `;
+    // Here we appendChild the newly created div to the cartContent variable declared earlier.
+    // The appendChild method appends a node as the last child of a node
+    cartContent.appendChild(div);
+  }
+
+  // Change css class to show the cart
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
   }
 }
 
@@ -124,6 +191,19 @@ class Storage {
     // The localStorage property allow to save key/value pairs in a web browser
     // The JSON.stringify() method converts a JS object or value to a JSON string
     localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  // get products from local storage. It takes an id as argument.
+  // It is through local storage because we stored the products in the local storage when DOMContentLoadd
+  static getProduct(id) {
+    // We need to parse since we stored the products as string. This will simply return the array from the local storage
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find((product) => product.id === id);
+  }
+
+  // This method will allow us to save the cart array in the local storage.
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
